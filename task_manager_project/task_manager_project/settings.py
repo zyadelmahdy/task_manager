c5a1eb05382c19e1fb3e653e1b62d51b495d7675
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-05-16 23:07:22 +0300

    commented the old app 'task_manager_app',

--- task_manager_project/task_manager_project/settings.py
+++ task_manager_project/task_manager_project/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager_app',
+    # 'task_manager_app',
     'task_project_manager.task_manager_project.task_manager_app',
 ]
 
