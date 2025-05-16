dffc96e712619f7751678304bfaf82e137c32a9f
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-05-17 00:08:09 +0300

    fixed STATIC_URL

--- task_manager_project/task_manager_project/settings.py
+++ task_manager_project/task_manager_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'task_manager_project.task_manager_project.task_manager_app.static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'task_manager_app', 'static'),
