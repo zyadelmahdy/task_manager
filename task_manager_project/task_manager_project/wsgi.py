5ac7631e8dddefd10156185a49acaf22b24e1c78
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-05-16 22:53:16 +0300

    updated path of the django settings module in wsgi

--- task_manager_project/task_manager_project/wsgi.py
+++ task_manager_project/task_manager_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager_project.settings')
 
 application = get_wsgi_application()
 
