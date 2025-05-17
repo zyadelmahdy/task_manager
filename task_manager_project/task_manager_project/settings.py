cd8f52ed0edd8eb8cf70ab2a01ecb1070eb3403b
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-05-17 15:49:04 +0300

    updated root_urlconf

--- task_manager_project/task_manager_project/settings.py
+++ task_manager_project/task_manager_project/settings.py
@@ -49,17 +49,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-# ROOT_URLCONF = 'task_manager_project.urls'
-ROOT_URLCONF = 'task_manager_project.task_manager_project.urls'
+ROOT_URLCONF = 'task_manager_project.urls'
+# ROOT_URLCONF = 'task_manager_project.task_manager_project.urls'
 
 
 
