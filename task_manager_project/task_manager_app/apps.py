bf0d03f005779634b1649ca063cf2aa55c706f86
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-06-01 23:51:23 +0300

    update render.yaml

--- task_manager_project/task_manager_app/apps.py
+++ task_manager_project/task_manager_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TaskManagerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_manager_app'  # This is correct
+    name = 'task_manager_app'
