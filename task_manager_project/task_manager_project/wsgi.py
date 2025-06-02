19e8cff45b147668e39def53a33475c873942bd2
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-06-02 23:04:33 +0300

    fix render.yaml and wsgi.py

--- task_manager_project/task_manager_project/wsgi.py
+++ task_manager_project/task_manager_project/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager_project.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager_project.settings')
 
 application = get_wsgi_application()
