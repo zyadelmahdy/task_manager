c427017a0e5adef2d6d8f401a5d665b5a1096636
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-06-02 22:38:54 +0300

    removed overriding docker command from render.yaml

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-# wait-for-db.sh
+# #!/bin/bash
+# # wait-for-db.sh
 
-set -e
+# set -e
 
-host="$1"
-shift
-cmd="$@"
+# host="$1"
+# shift
+# cmd="$@"
 
-until nc -z -v -w30 $host 5432; do
-  echo "Waiting for PostgreSQL at $host to start..."
-  sleep 1
-done
+# until nc -z -v -w30 $host 5432; do
+#   echo "Waiting for PostgreSQL at $host to start..."
+#   sleep 1
+# done
 
-echo "PostgreSQL is up - executing command"
-exec $cmd
+# echo "PostgreSQL is up - executing command"
+# exec $cmd
