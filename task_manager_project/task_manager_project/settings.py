c2dfe28cf72c2bc814b6ac237eea1637454b5d91
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-06-02 16:14:51 +0300

    added 'task_manager_app', in installed apps

--- task_manager_project/task_manager_project/settings.py
+++ task_manager_project/task_manager_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager_app',
     'task_manager_app.apps.TaskManagerAppConfig',  # Use the explicit app config
 ]
 
