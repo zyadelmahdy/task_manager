536bd2cca117114da4f002f64c41312537576746
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2025-06-03 15:17:24 +0300

    Update manage.py

--- task_manager_project/manage.py
+++ task_manager_project/manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager_project.settings')
+    # Dynamically choose settings based on whether you're on Render or local
+    if "RENDER" in os.environ:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager.settings')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_project.task_manager_project.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +19,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
